99a6ea7f984be231098267a0fb3014f9d467e8a4
Hedonysos <167946166+theserendipist@users.noreply.github.com>
2024-06-10 19:05:16 +0200

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let min, max;
 
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    } else if (a < 0 || b < 0) {
+        return 'ERROR';
+    } else if (a < b) {
+        min = a;
+        max = b;
+    } else {
+        min = b;
+        max = a;
+    }
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
